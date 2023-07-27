dbbe1106bb52690e626a9f2ba9211937b82aa423
fuad Salo <fuadsalo8@gmail.com>
2023-07-27 06:09:13 -0700

    adae

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	unsigned int len = 0;
-			
+
 	while (str[len])
 	len++;
 
@@ -22,9 +22,9 @@ list_t *add_node(list_t **head, const char *str)
 	return (NULL);
 
 	new->str = strdup(str);
- 	new->len = len;
- 	new->next = (*head);
+	new->len = len;
+	new->next = (*head);
 	(*head) = new;
-	
+
 	return (*head);
 }
